f451784620ea25959aee81ba1661ceea69c31782
Uriel Corfa <korfuri@gmail.com>
2017-06-18 17:36:19 +0800

    Go mode: support errcheck, and add an hydra for the mode on C-.

--- emacs.d/site-lisp/korfuri--golang.el
+++ emacs.d/site-lisp/korfuri--golang.el
@@ -7,6 +7,8 @@
 ;; * export PATH="$PATH:$GOPATH/bin"` in your bashrc
 ;; * `go get -u github.com/dougm/goflymake` for flycheck
 ;; * `go get -u github.com/nsf/gocode` for company & eldoc
+;; * `go get -u github.com/rogpeppe/godef` for godef
+;; * `go get -u github.com/kisielk/errcheck` for errcheck
 
 ;;; Code:
 
@@ -15,21 +17,46 @@
 
 ;; Flycheck
 (add-to-list 'load-path "$GOPATH/src/github.com/dougm/goflymake")
+
 (use-package go-flycheck)
 
 ;; Company
 (add-to-list 'load-path "$GOPATH/src/github.com/nsf/gocode/emacs-company")
+
 (use-package company-go
   :ensure t)
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)))
+
+(add-hook
+ 'go-mode-hook
+ (lambda ()
+   (set (make-local-variable 'company-backends) '(company-go))
+   (company-mode)))
 
 ;; Eldoc
 (use-package go-eldoc
   :ensure t
   :init (add-hook 'go-mode-hook 'go-eldoc-setup))
 
+;; Errcheck
+(use-package go-errcheck)
+
+;; Go mode hydra
+(define-key
+  go-mode-map (kbd "C-.")
+  (defhydra hydra-golang
+    (go-mode-map "C-.")
+    "Golang"
+    ("a" beginning-of-defun "beginning-of-defun")
+    ("e" end-of-defun "end-of-defun")
+    ("j" godef-jump "godef-jump")
+    ("d" godef-describe "godef-describe")
+    ("i a" go-import-add "import-add")
+    ("i r" go-remove-unused-imports "remove-unused-imports")
+    ("i j" go-goto-imports "jump-to-imports")
+    ("f" gofmt "gofmt")
+    ("d" godoc "godoc")
+    ("r" go-errcheck "errcheck")
+    ("q" nil "quit")))
 
 (provide 'korfuri--golang)
 ;;; korfuri--golang ends here
