f514ba475b53ee27e9c1322264e83123e4eae552
Uriel Corfa <korfuri@gmail.com>
2019-06-09 16:39:37 +0900

    Add [m]elpa packages directly so I do not depend on pulling code from the Internet

---
+++ emacs.d/elpa/treepy-0.1.1/treepy-autoloads.el
@@ -0,0 +1,15 @@
+;;; treepy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("treepy.el") (23732 16583 906355 660000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; treepy-autoloads.el ends here
